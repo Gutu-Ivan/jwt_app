3400772cf37c2a933ededf910526550b44ed43f9
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-13 23:54:07 +0300

    Created repositories

---
+++ src/main/java/dev/gutuivan/reposity/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.gutuivan.reposity;
+
+import dev.gutuivan.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String name);
+}
