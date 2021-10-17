248846e319f3f04b946332529cb8797139380b61
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-17 22:48:24 +0200

    Updated: *Service layer

--- src/main/java/dev/gutuivan/model/User.java
+++ src/main/java/dev/gutuivan/model/User.java
@@ -19,6 +19,54 @@ public class User extends BaseEntity{
     @Column(name = "email")
     private String email;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
     @Column(name = "password")
     private String password;
 
--- src/main/java/dev/gutuivan/service/impl/UserServiceImpl.java
+++ src/main/java/dev/gutuivan/service/impl/UserServiceImpl.java
@@ -1,35 +1,75 @@
 package dev.gutuivan.service.impl;
 
+import dev.gutuivan.model.Role;
+import dev.gutuivan.model.Status;
 import dev.gutuivan.model.User;
+import dev.gutuivan.reposity.RoleRepository;
+import dev.gutuivan.reposity.UserRepository;
 import dev.gutuivan.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
     @Override
     public User register(User user) {
-        return null;
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles = new ArrayList<>();
+        userRoles.add(roleUser);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
+        User registeredUser = userRepository.save(user);
+        log.info("IN register - user: {} succesfully registered", registeredUser);
+
+        return registeredUser;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User>  result = userRepository.findAll();
+        log.info("IN getAll - {} users found", result.size());
+        return result;
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUserName - user: {} found by username: {}", result,  username);
+        return result;
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        User result = userRepository.findById(id).orElse(null);
+        if (result == null){
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+        log.info("IN findById - user: {} found by id: {}", result);
+        return result;
     }
 
     @Override
     public void delete(Long id) {
-
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id: {} successfully deleted");
     }
 }
