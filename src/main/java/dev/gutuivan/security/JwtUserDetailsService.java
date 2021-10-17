2c862b2516d320c646c06744e4675c18a2f5d3d2
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-17 22:59:30 +0200

    Updated: *JwtUserDetailsService

--- src/main/java/dev/gutuivan/security/JwtUserDetailsService.java
+++ src/main/java/dev/gutuivan/security/JwtUserDetailsService.java
@@ -1,14 +1,30 @@
 package dev.gutuivan.security;
 
+import dev.gutuivan.model.User;
+import dev.gutuivan.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class JwtUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService (UserService userService){
+        this.userService = userService;
+    }
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if( user == null){
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
         return null;
     }
 }
