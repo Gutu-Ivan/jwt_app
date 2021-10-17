4ca55a32a3c41c28a73ac6d46d1d9593aaa908bf
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-17 23:59:04 +0200

    Created: +JwtUserFactory

--- src/main/java/dev/gutuivan/security/jwt/JwtUser.java
+++ src/main/java/dev/gutuivan/security/jwt/JwtUser.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 public class JwtUser implements UserDetails {
     private final Long id;
@@ -17,7 +18,7 @@ public class JwtUser implements UserDetails {
     private final Date lastPasswordResetDate;
     private final Collection<? extends GrantedAuthority> authorities;
 
-    public JwtUser(Long id, String username, String firstName, String lastName, String password, String email, boolean enabled, Date lastPasswordResetDate, Collection<? extends GrantedAuthority> authorities) {
+    public JwtUser(Long id, String username, String firstName, String lastName, String password, String email, List<GrantedAuthority> grantedAuthorities, boolean enabled, Date lastPasswordResetDate, Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.username = username;
         this.firstName = firstName;
