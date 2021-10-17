da40301950a1b9e92636a1c04db4a8a000174687
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-17 22:52:28 +0200

    Added: +JwtUserDetailsService

---
+++ src/main/java/dev/gutuivan/security/JwtUserDetailsService.java
@@ -0,0 +1,14 @@
+package dev.gutuivan.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
